9a21e67659f0a991100f1282ad501d89911d8a8f
Luiz Muniz <eusouluizeduardomuniz@gmail.com>
2023-02-22 11:09:51 -0300

    Feito as configurações iniciais do projeto MVC sem utilização de modelo padrão

---
+++ src/Dev.UI.App/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.UI.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Dev.UI.App/Program.cs
@@ -0,0 +1,31 @@
+namespace Dev.UI.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Tudo inicia a partir do builder
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Adicionando o MVC ao container
+            builder.Services.AddControllersWithViews();
+
+            // Realizando o buid das configurações que resultará na App
+            var app = builder.Build();
+
+            // Ativando a pagina de erros caso seja ambiente de desenvolvimento
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Adicionando Rota padrão
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            // Colocando a App para rodar
+            app.Run();
+        }
+    }
+}
